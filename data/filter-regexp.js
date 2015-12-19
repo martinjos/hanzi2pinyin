cc1fd5e5bd2dd8e2a81da80c8cb04191a1312ecd
Martin Sidaway <msidaway@gmail.com>
2015-12-19 23:47:22 +0000

    Best initial effort at converting punctuation.

--- data/filter-regexp.js
+++ data/filter-regexp.js
@@ -1,5 +1,5 @@
 function regexpConvertFilter(regexp, converter) {
-    return function (doc, child) {
+    return function (doc, child, isFirstNode, isLastNode) {
         var text = child.data;
 
         var pos = 0;
@@ -11,22 +11,31 @@ function regexpConvertFilter(regexp, converter) {
             var intermediate = text.substr(pos, matches.index - pos);
             if (intermediate.length > 0) {
                 result.push(document.createTextNode(intermediate));
+                isFirstNode = false;
             }
             pos = regexp.lastIndex;
             k = matches[0];
 
             r = converter.convert(k);
 
-            result.push(document.createTextNode(' '));
+            var lastWasPunct = isFirstNode;
             for (var i = 0; i < r.words.length; i++) {
-                if (i > 0)
-                    result.push(document.createTextNode(' '));
-                var span = document.createElement('span');
-                span.setAttribute('title', r.words[i]);
-                span.appendChild(document.createTextNode(r.pinyinWords[i]));
-                result.push(span);
+                if (r.words[i] == '') {
+                    result.push(document.createTextNode(r.pinyinWords[i]));
+                    lastWasPunct = true;
+                } else {
+                    if (!lastWasPunct)
+                        result.push(document.createTextNode(' '));
+                    var span = document.createElement('span');
+                    span.setAttribute('title', r.words[i]);
+                    span.appendChild(document.createTextNode(r.pinyinWords[i]));
+                    result.push(span);
+                    lastWasPunct = false;
+                }
             }
-            result.push(document.createTextNode(' '));
+            if (!lastWasPunct && (!isLastNode || pos < text.length))
+                result.push(document.createTextNode(' '));
+            isFirstNode = false;
         }
 
         if (pos == 0) {
--- data/h2p-converter.js
+++ data/h2p-converter.js
@@ -3,7 +3,72 @@ function H2PConverter(pinyinStr) {
     this.maxLength = 4;
 }
 
+var punctuation = {
+    '　': ' ',
+    '、': ', ',
+    '。': '. ',
+    '〈': ' ‹',
+    '〉': '› ',
+    '《': ' «',
+    '》': '» ',
+    '「': " ‘",
+    '」': "’ ",
+    '『': ' “',
+    '』': '” ',
+    '【': ' [',
+    '】': '] ',
+    '〔': ' (',
+    '〕': ') ',
+    '〖': ' {',
+    '〗': '} ',
+    '〘': ' {',
+    '〙': '} ',
+    '〚': ' [',
+    '〛': '] ',
+    '〜': '—',
+    '〝': ' “',
+    '〞': '” ',
+    '〟': '” ',
+    '︰': ': ',
+
+    // full-width punctuation
+    '！': '! ',
+    '＄': ' $',
+    '％': '% ',
+    '（': ' (',
+    '）': ') ',
+    '，': ', ',
+    '．': '. ',
+    '：': ': ',
+    '；': '; ',
+    '？': '? ',
+    '［': ' [',
+    '］': '] ',
+    '｛': ' {',
+    '｝': '} ',
+};
+
 H2PConverter.prototype.splicePinyin = function(first) {
+    var firstChar = this.current.charAt(0);
+    var firstCharCode = firstChar.charCodeAt(0);
+    if (punctuation[firstChar] !== undefined) {
+        this.current = this.current.substr(1);
+        this.words.push('');
+        this.pinyinWords.push(punctuation[firstChar]);
+        return;
+    } else if (firstCharCode >= 0xff01 && firstCharCode <= 0xff5e) {
+        // full-width form
+        var newCode = firstChar.charCodeAt(0) - 0xfee0;
+        var newForm = String.fromCharCode(newCode);
+        if (!(newCode >= 0x30 && newCode <= 0x39) &&
+            !(newCode >= 0x41 && newCode <= 0x5A) &&
+            !(newCode >= 0x61 && newCode <= 0x7A)) {
+            newForm = ' ' + newForm + ' ';
+        }
+        this.current = this.current.substr(1);
+        this.words.push('');
+        this.pinyinWords.push(newForm);
+    }
     for (var j = this.maxLength; j >= 1; j--) {
         var word = this.current.substr(0, j);
         var pinyinWord = null;
